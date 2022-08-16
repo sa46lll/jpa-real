3065eb08d382a862d3d736d11bb3ad3c7edaac6e
sa46lll <9812smh@naver.com>
2022-08-16 10:12:53 +0000

    chore: 프로젝트 생성

---
+++ src/main/java/com/sa46lll/jpareal/JparealApplication.java
@@ -0,0 +1,13 @@
+package com.sa46lll.jpareal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JparealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JparealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sa46lll/jpareal/JparealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa46lll.jpareal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JparealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
